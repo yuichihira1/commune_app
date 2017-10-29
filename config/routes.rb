ca7c4aae7b263cf99c1609ac052a4541d7ffd96b
hirayuichi <yhira5796@gmail.com>
2017-10-29 21:32:53 +0900

    deviseとUsers関連のルーティングを設定した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, :controllers => {
+                          :registrations => 'users/registrations',
+                          :sessions => 'users/sessions'
+                                      }
   root 'pages#index'
-  resources :pages
+  resources :users, only: [:index, :show] do
+    resources :pages
+  end
 end

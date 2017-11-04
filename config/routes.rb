65d63bdce6ca913a00d362a611f368715f3de898
hirayuichi <yhira5796@gmail.com>
2017-11-04 18:39:20 +0900

    セッション周辺のルーティングを設定した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-                          :registrations => 'users/registrations',
-                          :sessions => 'users'
-                        }
+  devise_for :users, skip: [:sessions]
+  as :user do
+		get 'login', to: 'devise/sessions#new', as: :new_user_session
+		post 'login', to: 'devise/sessions#create', as: :user_session
+		delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
   root 'pages#index'
   resources :users, only: [:index, :show] do
     resources :pages, only: [:new, :create, :show, :edit, :delete]

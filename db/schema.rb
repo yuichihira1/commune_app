549e21809b6e40293ccce7ac0f84477aa635ce18
hirayuichi <yhira5796@gmail.com>
2017-11-04 21:27:58 +0900

    Usersテーブルにカラムを追加した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171029135830) do
+ActiveRecord::Schema.define(version: 20171104121508) do
 
   create_table "pages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
@@ -47,6 +47,9 @@ ActiveRecord::Schema.define(version: 20171029135830) do
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string "member"
+    t.string "profile"
+    t.string "region"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

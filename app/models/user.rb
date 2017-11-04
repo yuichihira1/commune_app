f7052cc00d27aa70f8680b9abbbea2d52ae059cc
hirayuichi <yhira5796@gmail.com>
2017-11-04 21:34:59 +0900

    ppaperclipのサイズの記述ミスを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
   has_many :pages
-  has_attached_file :avatar, :styles => { :medium => "64x64>", :thumb => "64x64>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "64x64", :thumb => "64x64" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end

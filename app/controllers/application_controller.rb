f4d592128a9b33cf98c7bbea1aa816cc7f3c38a9
hirayuichi <yhira5796@gmail.com>
2017-11-04 21:29:18 +0900

    ユーザー作成時のストロングパラメーターにパラメーターを追加した。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,7 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :remember_me, :avatar) }
-      devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :email, :password, :remember_me) }
-      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password, :avatar) }
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :remember_me, :avatar, :member, :region, :profile) }
+      devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:name, :email, :password, :remember_me) }
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,23 @@ class UsersController < ApplicationController
   	@users = User.all
   end
 
-  def show
-  	@user = User.find(params[:id])
+  def new
+  	 @user = User.new
   end
+
+  def create
+  	@user = User.new(params[:user])
+  	@user.save
+    redirect_to new_user_path
+  end
+
+  def show
+  	@user = current_user
+  end
+
+  private
+
+   def user_params
+     params.require(:user).permit(:name, :email, :password, :password_confirmation, :remember_me, :avatar, :member, :region, :profile)
+   end
 end
